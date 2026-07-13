4a4eaeb9d8d844f42e95a876a711ccbc40f0f626
easyict <s.d.randiwela@gmail.com>
2026-07-13 08:21:44 +0530

    Add files via upload

---
+++ site/config.js
@@ -0,0 +1,19 @@
+window.APP_CONFIG = {
+  // Cloudflare Worker එක deploy කළ පසු ලැබෙන URL එක මෙහි දමන්න.
+  // උදා: https://easyict-chatbot-api.your-name.workers.dev
+  API_URL: "https://easyict-chatbot-api.samiru.workers.dev",
+
+  APP_NAME: "easyict Chat Bot",
+
+  // Firebase Console > Project settings > Your apps > Web app
+  // යටතේ ලැබෙන config values මෙහි paste කරන්න.
+  FIREBASE_CONFIG: {
+   apiKey: "AIzaSyCG-rd4l-qqlWg02uFhnyhN5twUHb758pg",
+  authDomain: "easyict-chatbot.firebaseapp.com",
+  projectId: "easyict-chatbot",
+  storageBucket: "easyict-chatbot.firebasestorage.app",
+  messagingSenderId: "1070886478673",
+  appId: "1:1070886478673:web:800d8da986f6fcb25b2970",
+  measurementId: "G-VS0Q5F1ZC8"
+  }
+};
